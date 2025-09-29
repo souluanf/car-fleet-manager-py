688471345c425dc463fc3e593dc5b38be9c77038
Luan Fernandes <souluanf@icloud.com>
2025-09-29 19:22:50 -0300

    feat(domain): implementar entidade Brand

---
+++ backend/src/main/java/dev/luanfernandes/domain/entity/Brand.java
@@ -0,0 +1,37 @@
+package dev.luanfernandes.domain.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "brands")
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    @OneToMany(mappedBy = "brand", cascade = CascadeType.ALL)
+    private List<Vehicle> vehicles = new ArrayList<>();
+}
