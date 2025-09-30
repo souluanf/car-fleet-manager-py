52ca6f39bb633c6f7dd3ab13c5d4bada448ea8b5
Luan Fernandes <souluanf@icloud.com>
2025-09-29 21:10:45 -0300

    feat(config): adicionar constantes de paths e mensagens

---
+++ backend/src/main/java/dev/luanfernandes/constants/OpenApiConfigConstants.java
@@ -0,0 +1,15 @@
+package dev.luanfernandes.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class OpenApiConfigConstants {
+    public static final String OPENAPI_INFO_TITLE = "Car Fleet API";
+    public static final String OPENAPI_INFO_VERSION = "0.0.1";
+    public static final String OPENAPI_INFO_DESCRIPTION = "API para gerenciamento de frotas de veículos.";
+    public static final String OPENAPI_CONTACT_NAME = "Luan Fernandes";
+    public static final String OPENAPI_CONTACT_EMAIL = "contact@luanfernandes.dev";
+    public static final String OPENAPI_CONTACT_URL = "https://linkedin.com/in/souluanf";
+    public static final String OPENAPI_EXTERNAL_DOCS_DESCRIPTION = "Git repository";
+    public static final String OPENAPI_EXTERNAL_DOCS_URL = "https://github.com/souluanf/car-fleet-manager";
+}
